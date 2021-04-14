200b009ae385aa13fea04b8558485f147b69e448
AlexOcampo4801 <wilfer.ocampo@est.iudigital.edu.co>
2021-04-14 02:26:15 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './componets/NavBar';
+import Table from './componets/Table';
 
 function App() {
+
+  
+  const texto = "Peliculas IUDigital"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+       <NavBar titulo={texto}/>
+       <Table/>
+    </div>   
   );
 }
 
